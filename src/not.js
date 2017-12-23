3f623389875be89cd54a23a79d6cf80b47454b1f
Derek <derek.dorr@gmail.com>
2017-12-23 19:41:52 +0000

    Update docs and increment version

--- src/not.js
+++ src/not.js
@@ -1,15 +1,17 @@
 /**
- * Returns false if truthy, true if falsy
+ * Returns false if truthy, true if falsy, negation if function
  *
  * @module not
  * @func
  * @since 1.1.0
  * @param {*} val
- * @return {boolean}
+ * @return {boolean|function}
  * @example
  *
+ *     const identity = a => a;
  *     not(1); //=> false
  *     not(false); //=> true
+ *     not(identity)(true); //=> false
  */
 
 import isFunction from './isFunction';
