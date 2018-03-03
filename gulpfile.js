39c5eda77b66b1e57d81643742119f89e2ddf527
Derek <derek.dorr@gmail.com>
2018-03-03 16:40:53 +0000

    Update dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
 const concat = require('gulp-concat');
  
 gulp.task('docs', () => gulp.src('lib/*.js')
     .pipe(concat('README.md'))
     .pipe(gulpJsdoc2md())
-    .on('error', function (err) {
-        gutil.log('jsdoc2md failed:', err.message)
-    })
     .pipe(gulp.dest('.')));
